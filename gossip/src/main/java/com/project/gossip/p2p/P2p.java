70bf54a870301e5854683e29b6373b0ab36fb877
Hamza Zafar <ga83jiy@mytum.de>
2017-05-30 13:15:53 +0200

    adding initial directory structure

---
+++ gossip/src/main/java/com/project/gossip/p2p/P2p.java
@@ -0,0 +1,13 @@
+package com.project.gossip.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class P2p
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World P2P Package!" );
+    }
+}

55b9b71901cc2b257a73ca9332a87355d4183209
Muhammad Bhatti <mdr.bhatti@tum.de>
2017-06-15 20:10:39 +0200

    Send list of available peers to client

--- gossip/src/main/java/com/project/gossip/bootstrap/BootStrapServer.java
+++ gossip/src/main/java/com/project/gossip/bootstrap/BootStrapServer.java
@@ -10,7 +10,7 @@ import java.util.Iterator;
 import java.nio.ByteBuffer;
 
 public class BootStrapServer{
-
+  /* TEST INTERACTIVELY: nc -u 127.0.0.1 54352 */
   private DatagramChannel serverSocket;
   private ByteBuffer buffer;
 
@@ -21,10 +21,11 @@ public class BootStrapServer{
   }
 
   public void listen() throws IOException{
-	  HashMap<Integer,String> hashMap = new HashMap<Integer,String>(); 
+    HashMap<Integer,String> hashMap = new HashMap<Integer,String>(); 
     while(true){
       InetSocketAddress clientAddress = (InetSocketAddress) this.serverSocket.receive(buffer);
       int hash = clientAddress.hashCode();
+
       //could happen
       if(clientAddress == null){
         System.out.println("Address was null");
@@ -38,9 +39,13 @@ public class BootStrapServer{
         System.out.print((char) buffer.get());
       }
       buffer.clear();
-      System.out.print("Hashmap so far:\n--------------------------------\n");
-      hashMap.forEach((k,v)-> System.out.println(k+", "+v));
-      System.out.print("--------------------------------\n");
+      // Send reply to client with  entries + Hash
+      StringBuilder b = new StringBuilder();
+      hashMap.forEach((k,v)-> b.append(k+", "+v+ "\n"));
+      buffer.put(b.toString().getBytes());
+      buffer.flip();
+      int bytesSent = serverSocket.send(buffer, clientAddress);
+      buffer.clear();
     }
   }
 
@@ -50,7 +55,7 @@ public class BootStrapServer{
       run bootstrap server
       for testing purpose use `cat README.md | nc -u 127.0.0.1 54352`
       to write to server. Currently server just outputs what is receives
-    */
+     */
     BootStrapServer server = new BootStrapServer(54352, "127.0.0.1");
     server.listen();
   }
